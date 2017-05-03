1617276d9c49107bf65df44581a4b420bdf0e85a
Einsbon Kim <einsbon@gmail.com>
2017-05-04 00:36:07 +0900

    초기 커밋.

---
+++ Optical_illusion_text/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Optical_illusion_text
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtLetter = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblFont = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.btnFont = new System.Windows.Forms.Button();
+            this.lblSize = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtHeiPercent = new System.Windows.Forms.TextBox();
+            this.lblPercent = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtWidPercent = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 172);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(900, 600);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtLetter
+            // 
+            this.txtLetter.Location = new System.Drawing.Point(180, 12);
+            this.txtLetter.Name = "txtLetter";
+            this.txtLetter.Size = new System.Drawing.Size(306, 26);
+            this.txtLetter.TabIndex = 6;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(805, 12);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(107, 26);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Save image";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(162, 19);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Write the letters";
+            // 
+            // lblFont
+            // 
+            this.lblFont.AutoSize = true;
+            this.lblFont.Location = new System.Drawing.Point(144, 52);
+            this.lblFont.Name = "lblFont";
+            this.lblFont.Size = new System.Drawing.Size(342, 19);
+            this.lblFont.TabIndex = 11;
+            this.lblFont.Text = "Font: (Recommendation: Consolas bold)";
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(492, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 26);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Generate image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnFont
+            // 
+            this.btnFont.Location = new System.Drawing.Point(12, 52);
+            this.btnFont.Name = "btnFont";
+            this.btnFont.Size = new System.Drawing.Size(124, 26);
+            this.btnFont.TabIndex = 13;
+            this.btnFont.Text = "Select font";
+            this.btnFont.UseVisualStyleBackColor = true;
+            this.btnFont.Click += new System.EventHandler(this.btnFont_Click);
+            // 
+            // lblSize
+            // 
+            this.lblSize.AutoSize = true;
+            this.lblSize.Location = new System.Drawing.Point(144, 71);
+            this.lblSize.Name = "lblSize";
+            this.lblSize.Size = new System.Drawing.Size(99, 19);
+            this.lblSize.TabIndex = 14;
+            this.lblSize.Text = "Font size:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(465, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 38);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Developer\'s blog :\r\nE-mail adress : ";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(642, 111);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(270, 19);
+            this.linkLabel1.TabIndex = 16;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "http://blog.naver.com/einsbon";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(642, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 19);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "einsbon@naver.com";
+            // 
+            // txtHeiPercent
+            // 
+            this.txtHeiPercent.Location = new System.Drawing.Point(201, 108);
+            this.txtHeiPercent.Name = "txtHeiPercent";
+            this.txtHeiPercent.Size = new System.Drawing.Size(75, 26);
+            this.txtHeiPercent.TabIndex = 18;
+            // 
+            // lblPercent
+            // 
+            this.lblPercent.AutoSize = true;
+            this.lblPercent.Location = new System.Drawing.Point(15, 111);
+            this.lblPercent.Name = "lblPercent";
+            this.lblPercent.Size = new System.Drawing.Size(180, 19);
+            this.lblPercent.TabIndex = 19;
+            this.lblPercent.Text = "Gap up and down (%)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(87, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 19);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Spacing (%)";
+            // 
+            // txtWidPercent
+            // 
+            this.txtWidPercent.Location = new System.Drawing.Point(201, 140);
+            this.txtWidPercent.Name = "txtWidPercent";
+            this.txtWidPercent.Size = new System.Drawing.Size(75, 26);
+            this.txtWidPercent.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 784);
+            this.Controls.Add(this.txtWidPercent);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblPercent);
+            this.Controls.Add(this.txtHeiPercent);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblSize);
+            this.Controls.Add(this.btnFont);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblFont);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtLetter);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Illusion Text";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtLetter;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblFont;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.Button btnFont;
+        private System.Windows.Forms.Label lblSize;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtHeiPercent;
+        private System.Windows.Forms.Label lblPercent;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtWidPercent;
+    }
+}
+
---
+++ Optical_illusion_text/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Optical_illusion_text
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static Bitmap DrawIllusionText(string writetext, Font asdfasdf, int heiPercent, int widPercent)
+        {
+            Bitmap mybitmap = new Bitmap(900, 600, PixelFormat.Format32bppRgb);
+            Graphics backColor = Graphics.FromImage(mybitmap);
+            backColor.FillRectangle(Brushes.Khaki, 0, 0, 900, 600);
+            backColor.Dispose();
+
+            writetext = writetext + "*";
+            int txtLength = writetext.Length;
+
+            //그래픽 생성 
+            Graphics graphics = Graphics.FromImage(mybitmap);
+
+            Font font = asdfasdf;
+            Font testFont = new Font(asdfasdf.Name, asdfasdf.Size);
+
+            //폰트 사이즈 재기 measuring font size
+            SizeF stringSize = graphics.MeasureString("A", font);
+
+            
+            int wid = (int)stringSize.Width;
+            int hei = (int)stringSize.Height;
+
+            int heiDistance = hei * heiPercent / 100;
+            int widDistance = wid * widPercent / 100;
+
+            int widLetter = 900 / widDistance;
+            int heightLetter = 600 / heiDistance;
+
+            int letterNumber;
+
+            //검은색 글씨 쓰기 write black letters
+            for (int b = 0; b < heightLetter; b++)
+            {
+                for (int a = 0; a < widLetter; a++)
+                {
+                    letterNumber = (txtLength * 3000 + a - b) % txtLength;
+                    switch ((8 + a % 8 - b % 8) % 8)
+                    {
+                        case 0:
+                            graphics.DrawString(Convert.ToString(writetext[letterNumber]), font, Brushes.Black, a * widDistance - widDistance / 10, b * heiDistance + heiDistance / 10);
+                            break;
+                        case 1:
+                            graphics.DrawString(Convert.ToString(writetext[letterNumber]), font, Brushes.Black, a * widDistance, b * heiDistance + heiDistance / 10);
+                            break;
+                        case 2:
+                            graphics.DrawString(Convert.ToString(writetext[letterNumber]), font, Brushes.Black, a * widDistance + widDistance / 10, b * heiDistance + heiDistance / 10);
+                            break;
+                        case 3:
+                            graphics.DrawString(Convert.ToString(writetext[letterNumber]), font, Brushes.Black, a * widDistance + widDistance / 10, b * heiDistance);
+                            break;
+                        case 4:
+                            graphics.DrawString(Convert.ToString(writetext[letterNumber]), font, Brushes.Black, a * widDistance + widDistance / 10, b * heiDistance - heiDistance / 10);
+                            break;
+                        case 5:
+                            graphics.DrawString(Convert.ToString(writetext[letterNumber]), font, Brushes.Black, a * widDistance, b * heiDistance - heiDistance / 10);
+                            break;
+                        case 6:
+                            graphics.DrawString(Convert.ToString(writetext[letterNumber]), font, Brushes.Black, a * widDistance - widDistance / 10, b * heiDistance - heiDistance / 10);
+                            break;
+                        case 7:
+                            graphics.DrawString(Convert.ToString(writetext[letterNumber]), font, Brushes.Black, a * widDistance - widDistance / 10, b * heiDistance);
+                            break;
+                    }
+                }
+            }
+
+            //하얀색 글씨 쓰기 write white letters
+            for (int b = 0; b < heightLetter; b++)
+            {
+                for (int a = 0; a < widLetter; a++)
+                {
+                    letterNumber = (txtLength * 1000 + a - b) % txtLength;
+                    switch ((8 + a % 8 - b % 8) % 8)
+                    {
+                        case 4:
+                            graphics.DrawString(Convert.ToString(writetext[letterNumber]), font, Brushes.White, a * widDistance - widDistance / 10, b * heiDistance + heiDistance / 10);
+                            break;
+                        case 5:
+                            graphics.DrawString(Convert.ToString(writetext[letterNumber]), font, Brushes.White, a * widDistance, b * heiDistance + heiDistance / 10);
+                            break;
+                        case 6:
+                            graphics.DrawString(Convert.ToString(writetext[letterNumber]), font, Brushes.White, a * widDistance + widDistance / 10, b * heiDistance + heiDistance / 10);
+                            break;
+                        case 7:
+                            graphics.DrawString(Convert.ToString(writetext[letterNumber]), font, Brushes.White, a * widDistance + widDistance / 10, b * heiDistance);
+                            break;
+                        case 0:
+                            graphics.DrawString(Convert.ToString(writetext[letterNumber]), font, Brushes.White, a * widDistance + widDistance / 10, b * heiDistance - heiDistance / 10);
+                            break;
+                        case 1:
+                            graphics.DrawString(Convert.ToString(writetext[letterNumber]), font, Brushes.White, a * widDistance, b * heiDistance - heiDistance / 10);
+                            break;
+                        case 2:
+                            graphics.DrawString(Convert.ToString(writetext[letterNumber]), font, Brushes.White, a * widDistance - widDistance / 10, b * heiDistance - heiDistance / 10);
+                            break;
+                        case 3:
+                            graphics.DrawString(Convert.ToString(writetext[letterNumber]), font, Brushes.White, a * widDistance - widDistance / 10, b * heiDistance);
+                            break;
+                    }
+                }
+            }
+
+            //보라색 글씨 쓰기 Write purple letters
+            for (int b = 0; b < heightLetter; b++)
+            {
+                for (int a = 0; a < widLetter; a++)
+                {
+                    letterNumber = (txtLength * 1000 + a - b) % txtLength;
+                    graphics.DrawString(Convert.ToString(writetext[letterNumber]), font, Brushes.MediumSlateBlue, a * widDistance, b * heiDistance);
+                }
+            }
+
+
+            graphics.Dispose();
+            return mybitmap;     //return Bitmap Image 
+        }
+
+        //이미지 생성 버튼
+        //Generate image button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                pictureBox1.Image = DrawIllusionText(txtLetter.Text, myfont, Convert.ToInt16(txtHeiPercent.Text), Convert.ToInt16(txtWidPercent.Text));
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+                return;
+            }
+        }
+
+        //Save 버튼
+        //Save button
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string fileName;
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = "Save an Image File";
+            saveFileDialog.OverwritePrompt = true;
+            saveFileDialog.Filter = "JPEG File(*.jpg)|*.jpg |Bitmap File(*.bmp)|*.bmp |PNG File(*.png)|*.png";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                fileName = saveFileDialog.FileName;
+                pictureBox1.Image.Save(fileName);
+            }
+        }
+
+        Font myfont;
+
+
+        //폰트 선택 버튼 select font button
+        private void btnFont_Click(object sender, EventArgs e)
+        {
+            if(fontDialog1.ShowDialog() == DialogResult.OK)
+            {
+                myfont = fontDialog1.Font;/*
+                myFontName = myfont.Name;
+                myFontSize = myfont.Size;*/
+                lblFont.Text = "Font: " + myfont.Name;
+                lblSize.Text = "Font size: " + Convert.ToString(myfont.Size);
+                button1.Enabled = true;
+            }
+        }
+        
+        //패널 링크 바로가기
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            e.Link.LinkData = "http://blog.naver.com/einsbon";
+            System.Diagnostics.Process.Start(e.Link.LinkData.ToString());
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            e.Link.LinkData = "einsbon@naver.com";
+        }
+    }
+}
